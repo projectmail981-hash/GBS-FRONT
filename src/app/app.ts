4cffb317a23a5d5d5bd894f9a449ccf8f4ba8d79
projectmail981-hash <projectmail981@gmail.com>
2026-09-09 15:24:26 +0530

    Fix hardware back button closing the app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
-import { Component, signal } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-
+import { Component, OnInit, NgZone } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { App as CapacitorApp } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,24 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   templateUrl: './app.html'
 })
-export class App {
-  
+export class App implements OnInit {
+  constructor(
+    private location: Location,
+    private router: Router,
+    private zone: NgZone
+  ) {}
+
+  ngOnInit() {
+    CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+      this.zone.run(() => {
+        // If we are on the dashboard, exit the app
+        if (this.router.url === '/dashboard' || this.router.url === '/') {
+          CapacitorApp.exitApp();
+        } else {
+          // Otherwise, go back to the previous page
+          this.location.back();
+        }
+      });
+    });
+  }
 }
